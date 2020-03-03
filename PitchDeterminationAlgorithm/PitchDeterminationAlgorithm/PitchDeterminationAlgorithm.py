fb513e811820644376fb2a7940107adc9e977f2f
RichardHemphill <richardlhemphill@gmail.com>
2020-03-02 21:19:25 -0500

    more refactoring

--- PitchDeterminationAlgorithm/PitchDeterminationAlgorithm/PitchDeterminationAlgorithm.py
+++ PitchDeterminationAlgorithm/PitchDeterminationAlgorithm/PitchDeterminationAlgorithm.py
@@ -1,56 +1,39 @@
-import time
-from time import process_time
-import importlib
+#import time
+#from time import process_time
+#import importlib
 from tkinter import *
-from tkinter import messagebox
-from tkinter.filedialog import askopenfilename
-import amfm_decompy.pYAAPT as pYAAPT
-import amfm_decompy.basic_tools as basic
+#from tkinter import messagebox
+#from tkinter.filedialog import askopenfilename
+#import amfm_decompy.pYAAPT as pYAAPT
+#import amfm_decompy.basic_tools as basic
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.backend_bases import key_press_handler
+#from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-import matplotlib.pyplot as pplt 
-import numpy as np
-import wave
-import sys
-import os
-import pyaudio
+#import matplotlib.pyplot as pplt 
+#import numpy as np
+#import wave
+#import sys
+#import os
+#import pyaudio
 
 class PitchDeterminationAlgorithm(object):
 
-    # Constants
-    PDA_TITLE="Pitch Determination Algorithm"   # window title
-    WINDOW_GEOMETRY='640x480'                   # default size
-    WINDOW_PERCENT_SCREEN=0.75                  # use 75% screen width and height
-    PROCESSING_MODES={"GPU":1,"CPU":2}
-    INPUT_SOURCES={"MIC":1,"FILE":2}
-    DEFAULT_SOURCE_FILE="Source Sound File"
-    RATE = 4800
-    CHUNK = int(RATE/20) # RATE / number of updates per second
-
-    # Member Variables
-
-
     # Inner Classes
-    class PdaInner:
-        def __str__(self):
-            return type(self).__name__
-
-    class Gui(PdaInner):
+    class Gui:
         def __init__(self):
             self.window=Tk()
             self.commandForm=PitchDeterminationAlgorithm.CommandForm(self.window)
             self.pdaPlots=PitchDeterminationAlgorithm.PdaPlots(self.window)
 
-    class CommandForm(PdaInner):
+    class CommandForm:
         def __init__(self, win):
             pass
 
-    class ProcessorSwitch(PdaInner):
+    class ProcessorSwitch:
         def __init__(self, frm):
             pass
 
-    class MagnitudePlot(PdaInner):
+    class MagnitudePlot:
         XLABEL_STR = 'Samples'
         YLABEL_STR = 'Magnitude'
         LABEL_FONTSIZE = 12
@@ -61,17 +44,20 @@ class PitchDeterminationAlgorithm(object):
             self.plt.set_ylabel(self.YLABEL_STR, fontsize=self.LABEL_FONTSIZE)
 
         def update(self, data):
+            self.data = data
+
+        def display(self):
             self.plt.clear()
-            self.plt.plot(data)
+            self.plt.plot(self.data)
             self.plt.axis([0,len(data),-2**16/2,2**16/2])
 
-    class FrequencyPlot(PdaInner):
+    class FrequencyPlot:
         XLABEL_STR = 'Time'
         YLABEL_STR = 'Frequency (Hz)'
         LABEL_FONTSIZE = 12
         AUTOSCALE_ENABLE = True
         AUTOSCALE_AXIS = 'x'
-        AUTOSCALE_TIGHT = TRUE
+        AUTOSCALE_TIGHT = True
 
         def __init__(self, plt, rate=44100, nttf=1024, noverlap=900, cmap='gray_r'):
             self.plt = plt.add_subplot(312)
@@ -83,10 +69,13 @@ class PitchDeterminationAlgorithm(object):
             self.cmap = cmap
 
         def update(self, data):
-            self.plt.specgram(data, NFFT=self.nttf, Fs=self.rate, noverlap=self.noverlap, cmap=self.cmap)
+            self.data = data
+
+        def display(self):
+            self.plt.specgram(self.data, NFFT=self.nttf, Fs=self.rate, noverlap=self.noverlap, cmap=self.cmap)
             self.plt.autoscale(enable=self.AUTOSCALE_ENABLE, axis=self.AUTOSCALE_AXIS, tight=self.AUTOSCALE_TIGHT)
 
-    class PitchPlot(PdaInner):
+    class PitchPlot:
         XLABEL_STR = 'Samples'
         YLABEL_STR = 'Pitch (Hz)'
         LABEL_FONTSIZE = 12
@@ -99,7 +88,7 @@ class PitchDeterminationAlgorithm(object):
         LEGEND_LOC = 'upper right'
         AUTOSCALE_ENABLE = True
         AUTOSCALE_AXIS = 'x'
-        AUTOSCALE_TIGHT = TRUE
+        AUTOSCALE_TIGHT = True
 
         def __init__(self, plt, rate=44100):
             self.plt = plt.add_subplot(313)
@@ -108,20 +97,22 @@ class PitchDeterminationAlgorithm(object):
             self.rate = rate
 
         def update(self, data):
-            self.plt.clear()
             basicSignal = basic.SignalObj(data, self.rate)
-            pitch = pYAAPT.yaapt(basicSignal)
-            self.plt.plot(pitch.values, label=self.PITCH_LABEL, color=self.PITCH_COLOR)
-            pitch.set_values(pitch.samp_values, len(pitch.values), interp_tech='step')
-            self.plt.plot(pitch.values, label=self.STEP_LABEL, color=self.STEP_COLOR)
-            pitch.set_values(pitch.samp_values, len(pitch.values), interp_tech='spline')
-            self.plt.plot(pitch.values, label=self.SPLINE_LABEL, color=self.SPLINE_COLOR)
+            self.pitch = pitch = pYAAPT.yaapt(basicSignal)
+            self.pitchStep = pitch.set_values(pitch.samp_values, len(pitch.values), interp_tech='step')
+            self.pitchSpline = pitch.set_values(pitch.samp_values, len(pitch.values), interp_tech='spline')
+
+        def display(self):
+            self.plt.clear()
+            self.plt.plot(self.pitch.values, label=self.PITCH_LABEL, color=self.PITCH_COLOR)
+            self.plt.plot(self.pitchStep.values, label=self.STEP_LABEL, color=self.STEP_COLOR)
+            self.plt.plot(self.pitchSpline.values, label=self.SPLINE_LABEL, color=self.SPLINE_COLOR)
             self.plt.legend(loc=self.LEGEND_LOC)
             self.plt.grid
             self.plt.axes.set_ylim([0,1000])
             self.plt.autoscale(enable=self.AUTOSCALE_ENABLE, axis=self.AUTOSCALE_AXIS, tight=self.AUTOSCALE_TIGHT)
 
-    class PdaPlots(PdaInner):
+    class PdaPlots:
         TITLE_STR = 'Signal'
         TITLE_FONTSIZE = 14
 
