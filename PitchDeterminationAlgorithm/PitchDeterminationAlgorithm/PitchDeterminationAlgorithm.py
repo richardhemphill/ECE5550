c1e44c4724a4756826b834739ffc6fe4184ec05d
RichardHemphill <richardlhemphill@gmail.com>
2020-03-06 09:39:10 -0500

    comments and additional classes

--- PitchDeterminationAlgorithm/PitchDeterminationAlgorithm/PitchDeterminationAlgorithm.py
+++ PitchDeterminationAlgorithm/PitchDeterminationAlgorithm/PitchDeterminationAlgorithm.py
@@ -1,3 +1,16 @@
+#==============================================================================
+# File: PitchDeterminationAlgorithm.py
+# Author: Richard Hemphill
+# ID: 903877709
+# Class: ECE5550 High Performance Computing
+# Teacher: Dr. Venton Kepuska
+# Project: Perform sound analysis with ability to utilize GPU or CPU for 
+#   processing. There are 3 plots (magnitude, frequency, pitch).  The data
+#   comes from either sound file or microphone.  The window provides a GUI with
+#   command buttons for easier control.  An elapsed time is displayed for the 
+#   user to demonstrate GPU vs CPU performance.
+#==============================================================================
+
 #import time
 #from time import process_time
 #import importlib
@@ -16,27 +29,53 @@ from matplotlib.figure import Figure
 #import os
 #import pyaudio
 
+## Class containing functionality needed for Pitch Determination Algorithm
 class PitchDeterminationAlgorithm(object):
 
-    # Inner Classes
-    class Gui:
+    # Public PitchDeterminationAlgorithm Members
+
+    ## Constructor
+    def __init__(self):
+        self.gui=PitchDeterminationAlgorithm.GUI()
+
+    ## Starts the Pitch Determination Algorithm
+    def run(self):
+        self.gui.run()
+
+    #####################
+    ### Inner Classes ###
+    #####################
+
+    ### GUI Classes ###
+
+    ## Class that sets up and manages the graphical interface.
+    class GUI(object):
+        # Constants
         PDA_TITLE="Pitch Determination Algorithm"   # window title
         WINDOW_PERCENT_SCREEN=0.75                  # use 75% screen width and height
 
+        # Public GUI Members
+
+        ## Constructor
         def __init__(self):
             self.window=Tk()
             self.__configureWindow(self.window)
             self.commandForm=PitchDeterminationAlgorithm.CommandForm(self.window)
             self.pdaPlots=PitchDeterminationAlgorithm.PdaPlots(self.window)
 
+        ## Activates the GUI
         def run(self):
             self.window.mainloop()
 
+        # Private GUI Members
+
+        ## Initializes the look of the GUI
         def __configureWindow(self, window):
             window.geometry(self.__windowSize(window))
             window.resizable(width=True, height=True)
             window.title(self.PDA_TITLE)
 
+        ## Determines the size the GUI window
         def __windowSize(self,window):
             # get screen size
             screenWidth=window.winfo_screenwidth()
@@ -49,56 +88,78 @@ class PitchDeterminationAlgorithm(object):
             # format to string that for geometry call
             return "{}x{}".format(windowWidth,windowHeight)
 
-    class CommandForm:
+    ## GUI Controls ##
+
+    ## Class that manages the window form for controlling the GUI.
+    class CommandForm(object):
+
+        ## Constructor
         def __init__(self, window):
-            f=Frame(window)
-            self.processorSwitch = PitchDeterminationAlgorithm.ProcessorSwitch(f)
-            self.sourceSwitch = PitchDeterminationAlgorithm.SourceSwitch(f)
-            self.fileBrowser = PitchDeterminationAlgorithm.FileBrowser(f)
-            f.pack(anchor=N, fill=X, expand=YES)
+            self.frame=Frame(window)
+            self.processorSwitch = PitchDeterminationAlgorithm.ProcessorSwitch(self.frame)
+            self.sourceSwitch = PitchDeterminationAlgorithm.SourceSwitch(self.frame)
+            self.fileBrowser = PitchDeterminationAlgorithm.FileBrowser(self.frame)
+            self.frame.pack(anchor=N, fill=X, expand=YES)
 
-    class CommandSwitch:
+    ## Genralized class for toggle switched in the GUI.
+    class CommandSwitch(object):
         DEFAULT_MODE=1              # default to GPU mode
-        DEFAULT_COLOR='light blue'  # default color to blue
 
-        def __init__(self, frm, modes, default=DEFAULT_MODE, color=DEFAULT_COLOR):
-           f=Frame(frm)
-           self.switch = IntVar(frm,self.DEFAULT_MODE)
-           for (text, mode) in modes.items():
-                r = Radiobutton(f, text=text, variable=self.switch,
-                    value=mode, indicator=0, background=color)
-                r.pack(side=LEFT)
-           f.pack(side=LEFT, padx=10)
+        ## Constructor
+        def __init__(self, form, modes, default=DEFAULT_MODE, color=None):
+           self.frame=Frame(form)
+           self.switch = IntVar(form,self.DEFAULT_MODE)
+           for (text, mode) in modes.items():           # itterate thru modes
+                r = Radiobutton(master=self.frame)      # parent widget
+                r.config(text=text)                     # button display text
+                r.config(variable=self.switch)          # switch variable
+                r.config(value=mode)                    # value of switch
+                r.config(indicatoron=False)             # raised/sunken button
+                if color is not None: r.config(background=color) # set color
+                r.pack(side=LEFT)                       # next left within parent widget
+           self.frame.pack(side=LEFT, padx=10)
 
+    ## Class for toggling between which type of process will handle calculations.
     class ProcessorSwitch(CommandSwitch):
         MODES={"GPU":1,"CPU":2}
         DEFAULT_MODE=1              # default to GPU mode
         SWITCH_COLOR='light blue'
 
+        ## Constructor
         def __init__(self, form):
-            super().__init__(form, self.MODES, self.DEFAULT_MODE, self.SWITCH_COLOR)
+            super().__init__(form=form, modes=self.MODES, default=self.DEFAULT_MODE, color=self.SWITCH_COLOR)
 
+    ## Class for toggling between source for auditory data.
     class SourceSwitch(CommandSwitch):
         MODES={"MIC":1,"FILE":2}
         DEFAULT_MODE=2              # default to File mode
         SWITCH_COLOR='pink'
 
+        ## Constructor
         def __init__(self, form):
-            super().__init__(form, self.MODES, self.DEFAULT_MODE, self.SWITCH_COLOR)
+            super().__init__(form=form, modes=self.MODES, default=self.DEFAULT_MODE, color=self.SWITCH_COLOR)
 
-    class FileBrowser:
-        def __init__(self, frm):
-            f=Frame(frm)
+    ## Class for selecting sound file.
+    class FileBrowser(object):
+
+        # Public FileBrowser Members
+
+        ## Constructor
+        def __init__(self, form):
+            self.frame=Frame(form)
             self.srcFile=StringVar()
-            self.prevSrcFile=''
-            l=Entry(f, textvariable=self.srcFile, justify=LEFT)
-            l.bind("<Return>", self.OnFileEntryClick)
-            l.pack(side=LEFT, fill=X, expand=YES)
-            b=Button(f, text="Browse", command=lambda:self.loadFile())
-            b.pack(side=LEFT)
-            f.pack(side=LEFT, padx=10, fill=X, expand=YES)
+            self.prevSrcFile=None
+            self.entry=Entry(self.frame, textvariable=self.srcFile, justify=LEFT)
+            self.entry.bind("<Return>", self.__OnFileEntryClick)
+            self.entry.pack(side=LEFT, fill=X, expand=YES)
+            self.button=Button(self.frame, text="Browse", command=lambda:self.__loadFile())
+            self.button.pack(side=LEFT)
+            self.frame.pack(side=LEFT, padx=10, fill=X, expand=YES)
 
-        def loadFile(self):
+        # Private FileBrowser Members
+
+        ## Open dialog window for finding/select file.
+        def __loadFile(self):
             fileName=askopenfilename(title = "Select sound file",
                 filetypes = (("Wave files","*.wav"), 
                              ("MP3 files","*.mp3")))
@@ -106,15 +167,16 @@ class PitchDeterminationAlgorithm(object):
             self.srcMode.set(self.INPUT_SOURCES.get('FILE'))
             #self.processSignals()
 
-        def OnFileEntryClick(self, event):
+        ## 
+        def __OnFileEntryClick(self, event):
             value=self.srcFile.get().strip()
             changed = True if self.prevSrcFile != value else False
             if changed:
                 self.srcMode.set(self.INPUT_SOURCES.get('FILE'))
-                self.processSignals()
+                #self.processSignals()
             self.prevSrcFile=value
 
-    class MagnitudePlot:
+    class MagnitudePlot(object):
         XLABEL_STR = 'Samples'
         YLABEL_STR = 'Magnitude'
         LABEL_FONTSIZE = 12
@@ -132,7 +194,7 @@ class PitchDeterminationAlgorithm(object):
             self.plt.plot(self.data)
             self.plt.axis([0,len(data),-2**16/2,2**16/2])
 
-    class FrequencyPlot:
+    class FrequencyPlot(object):
         XLABEL_STR = 'Time'
         YLABEL_STR = 'Frequency (Hz)'
         LABEL_FONTSIZE = 12
@@ -156,7 +218,7 @@ class PitchDeterminationAlgorithm(object):
             self.plt.specgram(self.data, NFFT=self.nttf, Fs=self.rate, noverlap=self.noverlap, cmap=self.cmap)
             self.plt.autoscale(enable=self.AUTOSCALE_ENABLE, axis=self.AUTOSCALE_AXIS, tight=self.AUTOSCALE_TIGHT)
 
-    class PitchPlot:
+    class PitchPlot(object):
         XLABEL_STR = 'Samples'
         YLABEL_STR = 'Pitch (Hz)'
         LABEL_FONTSIZE = 12
@@ -202,18 +264,13 @@ class PitchDeterminationAlgorithm(object):
             self.frame.pack(anchor=CENTER, fill=BOTH, expand=YES)
             self.plt=Figure()
             self.plt.suptitle(self.TITLE_STR, fontsize=self.TITLE_FONTSIZE)
-
             self.subPlots = list()
-
             self.mPlt = PitchDeterminationAlgorithm.MagnitudePlot(self.plt)
             self.subPlots.append(self.mPlt)
-
             self.fPlt = PitchDeterminationAlgorithm.FrequencyPlot(self.plt)
             self.subPlots.append(self.fPlt)
-
             self.pPlt = PitchDeterminationAlgorithm.PitchPlot(self.plt)
             self.subPlots.append(self.pPlt)
-
             self.canvas = FigureCanvasTkAgg(self.plt, master=self.frame)
 
         def update(self,data):
@@ -232,14 +289,22 @@ class PitchDeterminationAlgorithm(object):
         def __init__(self, frame):
             super(PitchDeterminationAlgorithm.GpuPlots,self).__init__(frame)
 
-    # PDA Constructor
-    def __init__(self):
-        self.gui=PitchDeterminationAlgorithm.Gui()
+    ### Processing Classes ###
 
-    # Main Run Loop
-    def run(self):
-        self.gui.run()
+    ## Class to handle pitch processing
+    #  This is a sigleton so that only one instance keeps processed data
+    class PitchProcessor(object):
+        __instance = None
 
+        ## Creates new object
+        def __new__(cls):
+            if cls.__instance is None:
+                cls.__instance = super(PitchProcessor, cls).__new__(cls)
+            return cls.__instance
+
+###################################
+### Designated Start of Program ###
+###################################
 if __name__ == '__main__':
-    pda = PitchDeterminationAlgorithm()
-    pda.run()
+    pda = PitchDeterminationAlgorithm()     # instantiate PDA object
+    pda.run()                               # activate PDA object
